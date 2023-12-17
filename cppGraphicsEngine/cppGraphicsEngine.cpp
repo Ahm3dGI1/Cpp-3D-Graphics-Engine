208f873d9c3cd9796a0b982f770cb36ea71cb3f7
Ahmed G. Ibrahim <ahmedgouda797@gmail.com>
2023-12-17 10:00:31 -0800

    part1

--- cppGraphicsEngine/cppGraphicsEngine.cpp
+++ cppGraphicsEngine/cppGraphicsEngine.cpp
@@ -16,6 +16,11 @@ struct mesh {
 	vector<triangle> tris;
 };
 
+struct mat4x4 {
+	float m[4][4] = { 0 };
+
+};
+
 
 class olcEngine3D : public olcConsoleGameEngine {
 public:
@@ -25,6 +30,20 @@ public:
 
 private:
 	mesh meshCube;
+	mat4x4 matProj;
+
+	void MultiplyMatrixVector(vec3d &i, vec3d &o, mat4x4 &m) {
+
+		o.x = i.x * m.m[0][0] + i.y * m.m[1][0] + i.z * m.m[2][0] + m.m[3][0];
+		o.y = i.x * m.m[0][1] + i.y * m.m[1][1] + i.z * m.m[2][1] + m.m[3][1];
+		o.z = i.x * m.m[0][2] + i.y * m.m[1][2] + i.z * m.m[2][2] + m.m[3][2];
+		float w = i.x * m.m[0][3] + i.y * m.m[1][3] + i.z * m.m[2][3] + m.m[3][3];
+
+		if (w != 0.0f)
+		{
+			o.x /= w; o.y /= w; o.z /= w;
+		}
+	}
 
 public:
 	// Has the scene
@@ -52,15 +71,59 @@ public:
 			{0.0f, 1.0f, 0.0f,		1.0f, 1.0f, 1.0f,		1.0f, 1.0f, 0.0f},
 
 			// Bottom
-			{0.0f, 0.0f, 1.0f,		0.0f, 1.0f, 1.0f,		0.0f, 1.0f, 0.0f},
-			{0.0f, 0.0f, 1.0f,		0.0f, 1.0f, 0.0f,		0.0f, 0.0f, 0.0f},
+			{1.0f, 0.0f, 1.0f,		0.0f, 0.0f, 1.0f,		0.0f, 0.0f, 0.0f},
+			{1.0f, 0.0f, 1.0f,		0.0f, 0.0f, 0.0f,		1.0f, 0.0f, 0.0f},
 		};
+
+		// Projection Matrix
+		float fNear = 0.1f;
+		float fFar = 1000.0f;
+		float fFov = 90.0f;
+		float fAspectRatio = (float)ScreenHeight() / ScreenWidth();
+		float fFovRad = 1.0f / tanf(fFov * 0.5f / 180.0f * 3.14159f);
+
+		matProj.m[0][0] = fAspectRatio * fFovRad;
+		matProj.m[1][1] = fFovRad;
+		matProj.m[2][2] = fFar/(fFar - fNear);
+		matProj.m[2][3] = -fFar * fNear / (fFar - fNear);
+		matProj.m[3][2] = 1.0f;
+		matProj.m[3][3] = 0.0f;
 
 		return true;
 	}
 
 	// Updates the screen every delta time
 	bool OnUserUpdate(float fElapsedTime) override {
+
+		Fill(0, 0, ScreenWidth(), ScreenHeight(), PIXEL_SOLID, FG_BLACK);
+
+		// Draw Triangles
+		for (auto tri : meshCube.tris) {
+			triangle triProjected;
+			MultiplyMatrixVector(tri.p[0], triProjected.p[0], matProj);
+			MultiplyMatrixVector(tri.p[1], triProjected.p[1], matProj);
+			MultiplyMatrixVector(tri.p[2], triProjected.p[2], matProj);
+
+
+			// Scale the Object
+			triProjected.p[0].x += 1.0f; triProjected.p[0].y += 1.0f;
+			triProjected.p[1].x += 1.0f; triProjected.p[1].y += 1.0f;
+			triProjected.p[2].x += 1.0f; triProjected.p[2].y += 1.0f;
+
+			triProjected.p[0].x *= 0.5f * (float)ScreenWidth();
+			triProjected.p[0].y *= 0.5f * (float)ScreenWidth();
+			triProjected.p[1].x *= 0.5f * (float)ScreenWidth();
+			triProjected.p[1].y *= 0.5f * (float)ScreenWidth();
+			triProjected.p[2].x *= 0.5f * (float)ScreenWidth();
+			triProjected.p[2].y *= 0.5f * (float)ScreenWidth();
+
+
+			DrawTriangle(triProjected.p[0].x, triProjected.p[0].y,
+						triProjected.p[1].x, triProjected.p[1].y,
+						triProjected.p[2].x, triProjected.p[2].y, PIXEL_SOLID, FG_WHITE);
+
+
+		}
 
 
 		return true;
@@ -71,8 +134,6 @@ int main() {
 	olcEngine3D demo;
 	if (demo.ConstructConsole(256, 240, 4, 4))
 		demo.Start();
-
-
 
 	return 0;
 }
